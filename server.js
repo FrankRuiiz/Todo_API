a79b81d677a87b93ac28040d74c00a346f444dc2
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-06-13 00:29:20 -0700

    Add todos and todos/id

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var app = express(); 
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Go get beer',
+	completed: false
+},
+{
+	id: 2,
+	description: 'Go to market',
+	completed: false
+},
+{
+	id: 3,
+	description: 'Clean room',
+	completed: true
+}];
+
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
+});
+
+// GET request to get all todos, well use /todos
+app.get('/todos', function(req, res) {
+	res.json(todos);  // the json method allows us to send the data without having to use json.stringify like you normally would 
+});
+
+// GET request to get a singel todo, todos/:id
+app.get('/todos/:id', function(req, res) {
+	//res.send('Asking for todo with id of ' + req.params.id);
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+	for(var i=0; i<todos.length; i+=1) {
+		if(todos[i].id === todoID) {
+			matchedTodo = todos[i];
+		}
+	}
+	if(matchedTodo) {
+		res.json(matchedTodo); 
+	} else {
+		res.status(404).send('Id not found.');
+	}
 });
 
 app.listen(PORT, function() {
