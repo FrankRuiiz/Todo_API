eb96359103ad8e6d0bd2479a19f74866f27bfb1a
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-06-16 10:46:30 -0700

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express(); 
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -21,22 +22,30 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
 	//res.send('Asking for todo with id of ' + req.params.id);
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-	for(var i=0; i<todos.length; i+=1) {
-		if(todos[i].id === todoID) {
-			matchedTodo = todos[i];
-		}
-	}
-	if(matchedTodo) {
-		res.json(matchedTodo); 
-	} else {
-		res.status(404).send('Id not found.');
-	}
+	var matchedTodo = _findWhere(todos, {id: todoID});
+
+	// for(var i=0; i<todos.length; i+=1) {
+	// 	if(todos[i].id === todoID) {
+	// 		matchedTodo = todos[i];
+	// 	}
+	// }
+	// if(matchedTodo) {
+	// 	res.json(matchedTodo); 
+	// } else {
+	// 	res.status(404).send('Id not found.');
+	// }
 });
 
 // POST /todos/
 app.post('/todos', function(req, res) {
-	var newTodo = req.body;
+	// uses _.pick to use only the description and completed fields 
+	var newTodo = _.pick(req.body, 'description', 'completed');
+	if (!_.isBoolean(newTodo.completed) || !_.isString(newTodo.description) || newTodo.description.trim().length === 0) {
+		return res.status(400).send(); // req cant be completed 
+	}
+	// set body.description to be trimmed value
+	newTodo.description = newTodo.description.trim();
+
 	newTodo.id = todoNextId;
 	console.log(newTodo);
 	todos.push(newTodo);
